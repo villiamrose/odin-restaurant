54d18c4816fc9b4a2b4783812a28c62b280315e6
villiamrose <villiamrose@live.com>
2023-04-06 09:14:04 +0800

    Display dish definition carousel on home screen

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './style.css';
 
-console.log('Hi there!');
+const slideContainer = document.getElementById("slide-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slideContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slideContainer.scrollLeft -= slideWidth;
+})
