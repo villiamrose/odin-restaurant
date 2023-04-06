70b81344175ace6434015d0f2b93f0d7efe29df5
villiamrose <villiamrose@live.com>
2023-04-06 22:42:20 +0800

    Dynamically generate homepage content

---
+++ src/homepage.js
@@ -0,0 +1,65 @@
+function buildHomepage() {
+  const slideContainer = document.createElement('div');
+  slideContainer.className = 'slide-container';
+  slideContainer.id = 'slide-container';
+  appendSlides(slideContainer);
+
+  const prevBtn = document.createElement('button');
+  prevBtn.className = 'slide-arrow';
+  prevBtn.id = 'slide-arrow-prev';
+  prevBtn.innerHTML = "&#8249;";
+
+  const nextBtn = document.createElement('button');
+  nextBtn.className = 'slide-arrow';
+  nextBtn.id = 'slide-arrow-next';
+  nextBtn.innerHTML = "&#8250;";
+  
+  const home = document.createElement('div');
+  home.className = 'home';
+  home.append(prevBtn, nextBtn, slideContainer);
+
+  const main = document.querySelector('.main');
+  main.append(home);
+}
+
+function appendSlides(container) {
+  const definitions = [
+    {
+      term: "Kare-kare",
+      description: "Kare-kare is a Philippine stew that features a thick savory peanut sauce. It is generally made from a base of stewed oxtail, beef tripe, pork hocks, calves' feet, pig's feet or trotters, various cuts of pork, beef stew meat, and occasionally offal."
+    },{
+      term: "Sinigang",
+      description: "Sinigang is a Filipino soup or stew characterized by its sour and savory taste. It is most often associated with tamarind, although it can use other sour fruits and leaves as the souring agent. It is one of the more popular dishes in Filipino cuisine."
+    },{
+      term: "Adobo",
+      description: "Adobo is a popular Filipino dish that involves meat, seafood, or vegetables marinated in vinegar, soy sauce, garlic, bay leaves, and black peppercorns, which is browned in oil, and simmered in the marinade."
+    }
+  ];
+  
+  definitions.forEach((definition) => {
+    const slide = buildSlide(definition);
+    container.append(slide);
+  });
+}
+
+function buildSlide(definition) {
+  const termElement = document.createElement('h1');
+  termElement.className = 'term';
+  termElement.textContent = definition.term;
+
+  const descElement = document.createElement('p');
+  descElement.className = 'description';
+  descElement.textContent = definition.description;
+
+  const defElement = document.createElement('div');
+  defElement.className = 'definition';
+  defElement.append(termElement, descElement);
+
+  const slideElement = document.createElement('div');
+  slideElement.className = 'slide';
+  slideElement.append(defElement);
+
+  return slideElement;
+}
+
+export { buildHomepage };
--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
 import './style.css';
+import { buildHomepage } from './homepage.js';
 
-const slideContainer = document.getElementById("slide-container");
-const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("slide-arrow-prev");
-const nextButton = document.getElementById("slide-arrow-next");
+function homepageHandler() {
+  const slideContainer = document.getElementById("slide-container");
+  const slide = document.querySelector(".slide");
+  const prevButton = document.getElementById("slide-arrow-prev");
+  const nextButton = document.getElementById("slide-arrow-next");
 
-nextButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  slideContainer.scrollLeft += slideWidth;
-});
+  function scrollRight() {
+    const slideWidth = slide.clientWidth;
+    slideContainer.scrollLeft += slideWidth;
+  }
 
-prevButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  slideContainer.scrollLeft -= slideWidth;
-})
+  function scrollLeft() {
+    const slideWidth = slide.clientWidth;
+    slideContainer.scrollLeft -= slideWidth;
+  }
+
+  function scrollBack() {
+    const slideCount = slideContainer.childNodes.length;
+    const slideWidth = slide.clientWidth * slideCount;
+    slideContainer.scrollLeft -= slideWidth;
+  }
+
+  nextButton.addEventListener("click", scrollRight);
+  prevButton.addEventListener("click", scrollLeft);
+
+  let count = 1;
+
+  setInterval(() => {
+    if(count < 3) {
+      scrollRight();
+      count++;
+    } else {
+      scrollBack();
+      count = 1;
+    }
+  }, 10000);
+}
+
+buildHomepage();
